32d4f2ef7548145d5019a1c7bc42884779be2cff
jkondo14 <jkondo@ucsd.edu>
2023-10-24 16:48:47 -0700

    Commit

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,18 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test 
+	public void testReverseInPlaceAgain() {
+    int[] input1 = { 5,6,7,8 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 8,7,6,5 }, input1);
+	}
+
+  @Test
+  public void testReversedAgain() {
+    int[] input1 = {1,5,6,9};
+    assertArrayEquals(new int[]{9,6,5,1}, ArrayExamples.reversed(input1));
+  }
+  
 }
